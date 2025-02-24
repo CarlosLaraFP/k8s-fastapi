40ba96da58a412cb7e842e38b3da236884521017
CarlosLaraFP <carloslarafp@gmail.com>
2025-02-24 08:53:28 -0800

    async client

--- tests/test_main.py
+++ tests/test_main.py
@@ -31,9 +31,10 @@ async def test_client():
 
 @pytest.mark.asyncio
 async def test_read_main(test_client):
-    response = await test_client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Hello, FastAPI running with Redis!"}
+    async with test_client as client:
+        response = await client.get("/")
+        assert response.status_code == 200
+        assert response.json() == {"message": "Hello, FastAPI running with Redis!"}
 
 
 @pytest.mark.asyncio
