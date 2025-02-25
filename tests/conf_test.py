12ddb2c149857c62e3ad639d03d828aa5f287bac
CarlosLaraFP <carloslarafp@gmail.com>
2025-02-25 03:57:55 -0800

    rolling update YAML

--- tests/conf_test.py
+++
@@ -1,7 +0,0 @@
-#import pytest
-
-# Set default async mode for pytest-asyncio
-# Ensures pytest-asyncio automatically detects and runs async functions
-#@pytest.fixture(scope="session", autouse=True)
-#def configure_asyncio():
-#    pytest.asyncio_mode = "auto"
