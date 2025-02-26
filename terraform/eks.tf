8d56dabb5c9a4c4f627b17f334b965a92b041e69
CarlosLaraFP <carloslarafp@gmail.com>
2025-02-25 16:40:01 -0800

    Deployed AWS EKS Terraform resources

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -8,10 +8,15 @@ module "eks" {
   vpc_id          = module.vpc.vpc_id
 }
 
+resource "aws_key_pair" "eks_key" {
+  key_name   = "eks-key"  # Automatically creates a key pair
+  public_key = file("~/.ssh/id_rsa.pub")  # Uses the local SSH public key
+}
+
 resource "aws_eks_node_group" "eks_nodes" {
   cluster_name    = module.eks.cluster_name
   node_group_name = "eks-worker-nodes"
-  node_role_arn   = module.eks.cluster_iam_role_arn
+  node_role_arn   = aws_iam_role.eks_node_role.arn
   subnet_ids      = module.vpc.private_subnets
 
   scaling_config {
@@ -23,7 +28,7 @@ resource "aws_eks_node_group" "eks_nodes" {
   instance_types = [var.node_instance_type]
 
   remote_access {
-    ec2_ssh_key = "my-key-pair"  # Ensure this key pair exists in AWS
+    ec2_ssh_key = aws_key_pair.eks_key.key_name
   }
 
   depends_on = [module.eks]
